ff5a84723c2dd5d646fec9d1c9691b9af6377402
Baris Ceviz <me@barisceviz.com>
2018-09-23 18:57:51 +0300

    Created project

---
+++ src/AzureStorageCLI/Program.cs
@@ -0,0 +1,23 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AzureStorageCLI
+{
+    
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var services = new ServiceCollection()
+                .AddSingleton<IConsole>(PhysicalConsole.Singleton)
+                .BuildServiceProvider();
+
+            var app = new CommandLineApplication<App>();
+            app.Conventions
+                .UseDefaultConventions()
+                .UseConstructorInjection(services);
+            return app.Execute(args);
+        }
+
+    }
+}
