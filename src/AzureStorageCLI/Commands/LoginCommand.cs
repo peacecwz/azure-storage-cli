8fb30841ce530fcfa9f991ac2192303e631ed6e9
Baris Ceviz <me@barisceviz.com>
2018-09-23 18:59:10 +0300

    Implemented login command

---
+++ src/AzureStorageCLI/Commands/LoginCommand.cs
@@ -0,0 +1,47 @@
+using System;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace AzureStorageCLI.Commands
+{
+    [Command(Name = "login", Description = "Login to Azure Storage")]
+    [HelpOption("-h|--help")]
+    public class LoginCommand
+    {
+        [Argument(0, "accountName")] 
+        public string Name { get; set; }
+        
+        [Argument(1, "accountKey")] 
+        public string Key { get; set; }
+
+        public int OnExecute(CommandLineApplication app)
+        {
+            try
+            {
+                if (Settings.IsExist(Name))
+                {
+                    Console.WriteLine($"Your {Name} is already exist");
+                    return (int)StatusCodes.UnknownError;
+                }
+
+                var credentials = new StorageCredentials(Name, Key);
+                var account = new CloudStorageAccount(credentials, true);
+                account.CreateCloudBlobClient();
+                var settings = new Settings()
+                {
+                    AccountName = Name,
+                    AccountKey = Key
+                };
+                settings.Save();
+                Console.WriteLine($"Your {Name} is added");
+            }
+            catch
+            {
+                Console.WriteLine("Unhandled exception");
+            }
+
+            return (int)StatusCodes.Success;
+        }
+    }
+}
